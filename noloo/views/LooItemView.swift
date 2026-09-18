dea535bbf7a1d39746658031de86b473f4d87916
danielkagemann <info@danielkagemann.name>
2026-09-18 15:18:13 +0200

    feat: added missing

--- noloo/views/LooItemView.swift
+++ noloo/views/LooItemView.swift
@@ -1,5 +1,5 @@
 //
-//  LooItemView.swift
+//  sourcecode.swift
 //  noloo
 //
 //  Created by Daniel Kagemann on 18.09.26.
@@ -8,16 +8,37 @@
 import SwiftUI
 
 struct LooItemView: View {
-    
     // input
     var item: Loo
     var action: () -> Void
-    
+
+    var waterOpacity: [Double] {
+        var opacity: [Double] = [1, 1]
+
+        if item.amount <= 20 {
+            opacity = [0.2, 0.5]
+        } else if item.amount <= 50 {
+            opacity = [0.5, 0.5]
+        } else if item.amount <= 100 {
+            opacity = [0.5, 1]
+        } else {
+            opacity = [1, 1]
+        }
+
+        return opacity
+    }
+
     var body: some View {
         VStack {
-            HStack (spacing: -4) {
-                Circle().fill(.blue).frame(width: 12)
-                Circle().fill(.blue.opacity(0.4)).frame(width: 12)
+            HStack(spacing: -4) {
+                Circle()
+                    .fill(.blue.opacity(waterOpacity[0]))
+                    .stroke(.black.opacity(0.05), lineWidth: 1)
+                    .frame(width: 12)
+                Circle()
+                    .fill(.blue.opacity(waterOpacity[1]))
+                    .stroke(.black.opacity(0.05), lineWidth: 1)
+                    .frame(width: 12)
             }
             Text("\(item.amount) ml")
                 .bold()
@@ -38,4 +59,7 @@ struct LooItemView: View {
 
 #Preview {
     LooItemView(item: .init(amount: 20)) {}
+    LooItemView(item: .init(amount: 50)) {}
+    LooItemView(item: .init(amount: 100)) {}
+    LooItemView(item: .init(amount: 250)) {}
 }
--- noloo/views/Today.swift
+++ noloo/views/Today.swift
@@ -14,18 +14,29 @@ struct Today: View {
     /// app storage for daily goal (in ml)
     @AppStorage("DailyLoo") private var dailyLoo: Int = 2000
 
+    @ViewBuilder
+    func Missing() -> some View {
+        let remaining = dailyLoo - value
+        if remaining > 0 {
+            Text("Only \(dailyLoo - value)ml missing. ").padding(.top, 8)
+        }
+    }
+
     var body: some View {
-        HStack {
-            VStack(alignment: .leading, spacing: -2) {
-                Text("Today")
-                    .font(.headline)
-                Text("\(value) ml").font(.title).bold()
-                Text("\(dailyLoo) ml")
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
+        VStack {
+            HStack {
+                VStack(alignment: .leading, spacing: -2) {
+                    Text("Today")
+                        .font(.headline)
+                    Text("\(value) ml").font(.title).bold()
+                    Text("von \(dailyLoo) ml")
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                    Missing()
+                }
+                Spacer()
+                Text("\((value * 100) / dailyLoo)%").bold().foregroundStyle(.secondary)
             }
-            Spacer()
-            Text("\((value * 100) / dailyLoo)%").bold().foregroundStyle(.secondary)
         }
         .padding()
     }
