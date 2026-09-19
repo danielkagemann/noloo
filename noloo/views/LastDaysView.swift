f0f4182031d2d73ff9b10333b3e27e5b00973e26
danielkagemann <info@danielkagemann.name>
2026-09-19 12:51:37 +0200

    feat: some more animations. feat: extensions for Loo model

---
+++ noloo/views/LastDaysView.swift
@@ -0,0 +1,18 @@
+//
+//  LastDaysView.swift
+//  noloo
+//
+//  Created by Daniel Kagemann on 19.09.26.
+//
+
+import SwiftUI
+
+struct LastDaysView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LastDaysView()
+}
